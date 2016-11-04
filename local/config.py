c403c9ee3c964b8fd6b76aa9c31c91837be2ebc5
Dongho Choi <DonghoChoi@users.noreply.github.com>
2016-11-04 04:49:48 -0400

    Update config.py

--- local/config.py
+++ local/config.py
@@ -7,17 +7,3 @@ Created on Tue Apr 26 02:06:33 2016
 
 directory_of_all_data = '/Users/donghochoi/Documents/Work/Sensors_Study/Dropbox/Apps/Funf In A Box/daily_process/data'
 #directory_of_all_data='/Users/donghochoi/Documents/Work/Exploration_Study/Dissertation/UserStudy_Nov/test'
-
-info2_server = {
-    'user': 'dc817',
-    'password':'Chlehdgh0717',
-    'host':'128.6.192.99'
-}
-
-exploration_db = {
-  'user': 'dc817',
-  'password': 'Chlehdgh0717',
-  'host': '128.6.192.99',
-  'database': 'ExplorationStudy',
-  'raise_on_warnings': True
-}
